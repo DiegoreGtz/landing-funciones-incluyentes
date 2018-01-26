4c81c986ce1af0aa89d689c6227ffce5ef77f555
Gerardo Herrera <gherrera@ia.com.mx>
2018-01-26 13:59:12 -0600

    maqueta funciones incluyentes v2

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,36 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	var s;
+	$(".nav-link-tab").on("click", function(a) {
 
-	console.log('loaded');
+		a.preventDefault();
+		$(this).parent().addClass("nav-item-active");
+		$(this).parent().siblings().removeClass("nav-item-active");
 
+		var 	t = $(this).attr("href"),
+					e = "sj-tabs-box--" + $(this).data("tab");
+
+
+		$(t).siblings().removeClass("is-active");
+		$(t).parent().removeClass(s).addClass(e);
+		$(t).addClass("is-active");
+
+		s = e
+
+	})
+
+
+	$(".close-this").on("click", function(s) {
+		var $tab = $('.nav-item');
+		$tab.removeClass("nav-item-active")
+		s.preventDefault(), $(".ap-slide").removeClass("is-active")
+	})
 });
-
-	
-// Calcular la altura del contenedor en base al header
-
-// (function () {
-// 	var $wrapper 			= $('.wrapper'),
-// 			$header 			= $('.header'),
-// 			$headerH 			= $header.outerHeight(true),
-
-// 			setHeight = function(){
-
-// 										$headerH 	= $header.outerHeight(true);
-// 										$wrapper.css({
-// 											'margin-top': $headerH +'px',
-// 											'min-height': 'calc(100vh - ' + $headerH + 'px)'
-// 										});
-
-// 									};
-
-
-// 	setHeight();
-
-// 	$(window).resize(function(){
-// 		setHeight();
-// 	});
-
-// })();
-
 
 
 
